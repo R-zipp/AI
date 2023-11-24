2e69b9adcb368493903bb18eefdc9bddb180fd7f
kjm <kjm1qqq@gmail.com>
2023-11-24 14:25:05 +0900

    Fix

--- R-zipp/Lib/file_download_and_upload.py
+++ R-zipp/Lib/file_download_and_upload.py
@@ -10,7 +10,7 @@ from botocore.exceptions import NoCredentialsError
 import Lib.const as const
 
 
-with open('resources/security/secret.json', 'r') as file:
+with open('C:/Users/user/Desktop/json/secret.json', 'r') as file:
     data = json.load(file)
 
 ACCESS_KEY = data['AWS_ACCESSKEY']
